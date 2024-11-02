41e424289bbf80bed05cf99fb470cf9c34f8b7d3
Jared Bruni <jaredbruni@gmail.com>
2024-11-01 20:43:05 -0700

    path

--- editor/main_window.cpp
+++ editor/main_window.cpp
@@ -642,7 +642,7 @@ void MainWindow::runExec() {
             proc = new QProcess(this);
             QStringList args;
             static game::Point p[] = { game::Point(640, 360), game::Point(1280, 720), game::Point(1920, 1080), game::Point(3840, 2160) };
-            args << "-p " << (QDir::currentPath() + "/scroller/assets") << "-l " << file_name << "-g " << graphics_file << "-b " << run_window->exec_bg->text();
+            args << "-p " << (QDir::currentPath() + "/assets") << "-l " << file_name << "-g " << graphics_file << "-b " << run_window->exec_bg->text();
             QStringList argsx;
             QString coord_str;
             QTextStream stream(&coord_str);
